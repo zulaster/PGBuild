ce9e1f9bc7c5cef67bac00f9009cd015f905cf54
zulaster <zulaster@gmail.com>
2018-02-19 14:33:16 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,28 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
-    
+
+    registerEvents: function() {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+    },
     
     initialize: function() {
         var self = this;
