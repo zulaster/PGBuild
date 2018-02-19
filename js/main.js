2ac3e9c03f8c1f6a9e4bc754130fb13b1b812f8b
zulaster <zulaster@gmail.com>
2018-02-19 12:26:16 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 var app = {
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -15,10 +8,6 @@ var app = {
         }
     },
     
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
     
     initialize: function() {
         var self = this;
