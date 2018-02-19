398f676008ddd3118937be80ff92219cf0ee78c8
zulaster <zulaster@gmail.com>
2018-02-19 12:25:04 +0800

    Update HomeView.js

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,6 +1,23 @@
 var HomeView = function(store) {
  
+    this.initialize = function() {
+        // Define a div wrapper for the view. The div wrapper is used to attach events.
+        this.el = $('<div/>');
+        this.el.on('keyup', '.search-key', this.findByName);
+    };
  
+    this.initialize();
+    
+    this.render = function() {
+        this.el.html(HomeView.template());
+        return this;
+    };
+ 
+    this.findByName = function() {
+        store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(HomeView.liTemplate(employees));
+        });
+    };
 }
  
 HomeView.template = Handlebars.compile($("#home-tpl").html());
