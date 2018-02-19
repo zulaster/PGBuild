46eae89ddd24236cf915e1f256b0568901b35fd0
zulaster <zulaster@gmail.com>
2018-02-19 11:15:05 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,10 +13,20 @@ var app = {
         });
     },
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+    
     initialize: function() {
         this.store = new MemoryStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+    
+    
 
 };
 
