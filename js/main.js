cd38619e05151cc246670d8ef33263cdbcefaa5c
zulaster <zulaster@gmail.com>
2018-02-19 15:05:33 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,15 +28,34 @@ var app = {
                 $(event.target).removeClass('tappable-active');
             });
         }
+        $(window).on('hashchange', $.proxy(this.route, this));
     },
+    
     
     initialize: function() {
         var self = this;
         this.registerEvents();
+        this.detailsURL = /^#employees\/(\d{1,})/;
         this.store = new MemoryStore(function() {
             $('body').html(new HomeView(self.store).render().el);
         });
+    },
+    
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+        }
     }
+
 };
 
 app.initialize();
