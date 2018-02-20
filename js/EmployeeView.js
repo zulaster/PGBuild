97b32ca46821a060f754973c7d54397498f0d900
zulaster <zulaster@gmail.com>
2018-02-20 10:19:41 +0800

    Update EmployeeView.js

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -5,12 +5,25 @@ var EmployeeView = function(employee) {
       return this;
     };
  
+    this.addLocation = function(event) {
+        event.preventDefault();
+        console.log('addLocation');
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
+            },
+            function() {
+                alert('Error getting location');
+            });
+        return false;
+    };
+    
     this.initialize = function() {
         this.el = $('<div/>');
     };
  
     this.initialize();
- 
+    this.el.on('click', '.add-location-btn', this.addLocation);
  }
  
 EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
