4841d7f89ec598c7a2877d77f6beceb30e986cad
zulaster <zulaster@gmail.com>
2018-02-22 13:38:19 +0800

    Update EmployeeView.js

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -18,12 +18,30 @@ var EmployeeView = function(employee) {
         return false;
     };
     
+    this.addToContacts = function(event) {
+        event.preventDefault();
+        console.log('addToContacts');
+        if (!navigator.contacts) {
+            app.showAlert("Contacts API not supported", "Error");
+            return;
+        }
+        var contact = navigator.contacts.create();
+        contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+        contact.phoneNumbers = phoneNumbers;
+        contact.save();
+        return false;
+    };
+    
     this.initialize = function() {
         this.el = $('<div/>');
     };
  
     this.initialize();
     this.el.on('click', '.add-location-btn', this.addLocation);
+    this.el.on('click', '.add-contact-btn', this.addToContacts);
  }
  
 EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
