62c2b41f8545c256c4ab6d6329c50ff1753a2af8
zulaster <zulaster@gmail.com>
2018-02-23 16:31:48 +0800

    Update EmployeeView.js

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -35,6 +35,30 @@ var EmployeeView = function(employee) {
         return false;
     };
     
+    this.changePicture = function(event) {
+        event.preventDefault();
+        if (!navigator.camera) {
+            app.showAlert("Camera API not supported", "Error");
+            return;
+        }
+        var options =   {   quality: 50,
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+                            encodingType: 0     // 0=JPG 1=PNG
+                        };
+
+        navigator.camera.getPicture(
+            function(imageData) {
+                $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+            },
+            function() {
+                app.showAlert('Error taking picture', 'Error');
+            },
+            options);
+
+        return false;
+    };
+    
     this.initialize = function() {
         this.el = $('<div/>');
     };
